a92113923ebc3caa27896ad50ee77bc7db9c16ae
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-07 22:38:50 +0530

    working on styling of transactions list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,57 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Course", amount: 385, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "Mobile Recharge", amount: 799, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: [
+          Card(
+            color: Colors.blue,
+            child: Container(
+              child: Text("Chart"),
+              width: double.infinity,
+            ),
+            elevation: 8,
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              elevation: 5,
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 1.4)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "₹${tx.amount}",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(tx.title,
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16, color: Colors.purple),
+                  ),
+                  Text(tx.date.toString(),
+                    style: TextStyle(fontSize: 13, color: Colors.grey[800]),
+                  )
+                ],)
+              ],),
+            );
+          }).toList())
+        ],
       ),
     );
   }
