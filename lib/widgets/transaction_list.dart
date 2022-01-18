34bdd32d639a8f0991fd7d0a05e69d2ae1588f8c
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-18 15:21:24 +0530

    improved chart bars and lists

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,57 +12,54 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: transactions.isEmpty ? Column(children: [
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          height: 250,
-          child: Image.asset("assets/images/no_transactions_yet.jpg", fit: BoxFit.cover,)),
-          SizedBox(
-            height: 30,
-          ),
-          Text("No transactions yet, Keep saving!", style: TextStyle(fontFamily: "OpenSans", fontSize: 18, fontWeight: FontWeight.bold),)
-      ],) : ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-        elevation: 5,
-        child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Theme.of(context).primaryColor, width: 1.4)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                "₹${transactions[index].amount.toStringAsFixed(2)}",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Text(
-                  transactions[index].title,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
-                      color: Colors.purple),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                    height: 250,
+                    child: Image.asset(
+                      "assets/images/no_transactions_yet.jpg",
+                      fit: BoxFit.cover,
+                    )),
+                SizedBox(
+                  height: 30,
                 ),
                 Text(
-                  DateFormat.yMMMd().format(transactions[index].date),
-                  style: TextStyle(fontSize: 13, color: Colors.grey[800]),
+                  "No transactions yet, Keep saving!",
+                  style: TextStyle(
+                      fontFamily: "OpenSans",
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
                 )
               ],
             )
-          ],
-        ),
-      ); 
-        },
-        itemCount: transactions.length,
-        ),);
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 6,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text("₹${transactions[index].amount}")),
+                    ),
+                  ),
+                  title: Text(transactions[index].title,
+                    style: TextStyle(fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
