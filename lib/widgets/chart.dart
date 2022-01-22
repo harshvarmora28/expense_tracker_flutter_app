e470adb8b6416948bba4d641403282a3463116f0
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-22 22:02:46 +0530

    added button to show total expenses

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -7,12 +7,11 @@ import 'package:flutter_complete_guide/widgets/chart_bar.dart';
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
-  
   // Defining Colors
   var bcgColor = const Color(0xff1D0D25);
   var addIconColor = const Color(0xff577FFF);
   var cardBcgColor = const Color(0x90302636);
-
+  var totalExpColor = const Color(0xffffd45e);
 
   Chart(this.recentTransactions);
 
@@ -46,7 +45,7 @@ class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        shape: RoundedRectangleBorder(
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
       ),
       color: cardBcgColor,
@@ -54,17 +53,54 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 24, horizontal: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupedTransactionValues.map((data) {
-            return Flexible(
-              fit: FlexFit.tight,
-              child: ChartBar(
-                data["day"],
-                data["amount"],
-                totalSpending == 0.0 ? 0.0 : (data["amount"] as double ) / totalSpending
-            ));
-          }).toList(),
+        child: Column(
+          children: [
+            Text(
+              "Expenses in the last 7 Days",
+              style: TextStyle(color: Colors.white60),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: groupedTransactionValues.map((data) {
+                return Flexible(
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                        data["day"],
+                        data["amount"],
+                        totalSpending == 0.0
+                            ? 0.0
+                            : (data["amount"] as double) / totalSpending));
+              }).toList(),
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(45.0),
+              ),
+              color: totalExpColor,
+              elevation: 6,
+              margin: EdgeInsets.only(top: 20, bottom: 3),
+              child: Padding(
+                padding: const EdgeInsets.all(7),
+                child: Container(
+                  width: 60,
+                  height: 25,
+                  child: FittedBox(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "₹900.56",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: bcgColor),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
