601e582ac6f9c158ee0ba405e08d8def261a55be
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-01-26 13:55:03 +0530

    added banner ad unit at the bottom of the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_complete_guide/widgets/new_transaction.dart';
 import 'package:flutter_complete_guide/widgets/transaction_list.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './models/transaction.dart';
 import 'package:flutter_complete_guide/widgets/chart.dart';
 import 'dart:convert';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await dotenv.load();
+
+  runApp(MyApp());
+  MobileAds.instance.initialize();
+}
+
+const int maxAttempts = 3;
 
 class MyApp extends StatelessWidget {
   @override
@@ -36,6 +45,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  BannerAd staticAd;
+  bool staticAdLoaded = false;
+  BannerAd inlineAd;
+  bool inlineAdLoaded = false;
+
+  static const AdRequest request = AdRequest(
+      // keywords: ["", ""],
+      // contentUrl: "",
+      // nonPersonalizedAds: false
+      );
+
+  void loadStaticBannerAd() {
+    staticAd = BannerAd(
+        size: AdSize.banner,
+        // adUnitId: BannerAd.testAdUnitId,
+        // adUnitId: "ca-app-pub-4327821905067090/7464473777",
+        adUnitId: dotenv.env["STATIC_AD_UNIT_ID"] ?? "STATIC_AD_UNIT_ID not found!",
+        listener: BannerAdListener(onAdLoaded: (ad) {
+          setState(() {
+            staticAdLoaded = true;
+          });
+        }, onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+
+          print("Ad failed to load ${error.message}");
+        }),
+        request: request);
+
+    staticAd.load();
+  }
+
+  void loadInlineBannerAd() {
+    inlineAd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: BannerAd.testAdUnitId,
+        listener: BannerAdListener(onAdLoaded: (ad) {
+          setState(() {
+            inlineAdLoaded = true;
+          });
+        }, onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+
+          print("Ad failed to load ${error.message}");
+        }),
+        request: request);
+
+    inlineAd.load();
+  }
+
   List<Transaction> _userTransactions = [
     // Transaction(
     //     id: "t1", title: "New Course", amount: 385, date: DateTime.now()),
@@ -101,6 +159,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     initSharedPreferences();
+    loadStaticBannerAd();
     super.initState();
   }
 
@@ -156,6 +215,16 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions, _userTransactions),
+            // If you want to display ad before the transactions list
+
+            // if (staticAdLoaded)
+            //   Container(
+            //     child: AdWidget(ad: staticAd),
+            //     width: staticAd.size.width.toDouble(),
+            //     height: staticAd.size.height.toDouble(),
+            //     alignment: Alignment.bottomCenter,
+            //   ),
+            //   SizedBox(height: 10,),
             TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
@@ -172,6 +241,16 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () => _startAddNewTransaction(context),
         ),
       ),
+
+      // Displaying ad
+      bottomNavigationBar: staticAdLoaded
+          ? Container(
+              child: AdWidget(ad: staticAd),
+              width: staticAd.size.width.toDouble(),
+              height: staticAd.size.height.toDouble(),
+              alignment: Alignment.bottomCenter,
+            )
+          : SizedBox(),
     );
   }
 }
